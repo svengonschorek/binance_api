5f32abd673a6f0f2a60282b1804870fd7d70b41d
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2023-01-13 14:47:58 +0100

    master: copy script to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,6 @@ RUN pip install python-binance \
     && pip install google-cloud-bigquery
 
 WORKDIR /srv/
+COPY /scripts/daily_export.py /srv/
+
+CMD ["python", "./daily_export.py"]
--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 SET var=%cd%
 
-docker run -it ^
+docker run ^
   --env-file .\config\env.list ^
   --volume %var%\config\google_credentials.json:/srv/google_key.json:ro ^
   --mount type=bind,source=%var%\scripts,target=/srv/scripts ^
